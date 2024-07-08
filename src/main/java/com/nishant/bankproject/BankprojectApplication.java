28bd40daac14dee65c81a20c26187cf2aba13dea
nishantandewar <nishantandewar@gamil.com>
2024-07-08 22:58:49 +0530

    Update BankprojectApplication.java

--- src/main/java/com/nishant/bankproject/BankprojectApplication.java
+++ src/main/java/com/nishant/bankproject/BankprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankprojectApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(BankprojectApplication.class, args);
 	}
 
